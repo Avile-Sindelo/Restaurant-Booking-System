99fd87cb48bd39d34ea9a4066901b913d5033941
Avile Sindelo <avilesindelo@gmail.com>
2023-09-11 12:26:07 +0200

    get tables functionality

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import pgp from "pg-promise";
 import exphbs from "express-handlebars";
 import bodyParser from "body-parser";
 import flash from "flash-express";
+import RestaurantTableBooking from "./database.js";
 
-const app = express()
+const app = express();
+const connectionString = process.env.DATABASE_URL || 'postgres://labtyadr:P4QwEz9XgTXGqz3KaIM_BuyfNeU6yxk8@tai.db.elephantsql.com/labtyadr?ssl=true';
+const postgresP = pgp();
+const db = postgresP(connectionString);
+const database = RestaurantTableBooking(db);
 
 app.use(express.static('public'));
 app.use(flash());
@@ -21,9 +26,9 @@ const handlebarSetup = exphbs.engine({
 app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
 
-app.get("/", (req, res) => {
-
-    res.render('index', { tables : [{}, {}, {booked : true}, {}, {}, {}]})
+app.get("/", async (req, res) => {
+    
+    res.render('index', { tables : await database.getTables()})
 });
 
 
